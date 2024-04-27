72cf54c62a4a3e5e65912c00377a50766502bf1f
Mayank Maurya <mayankmaurya@Mayanks-MacBook-Pro.local>
2024-04-27 22:05:17 +0530

    removed-unnecessary code

--- src/components/hoc/AuthWrapper.jsx
+++ src/components/hoc/AuthWrapper.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-// import { useDispatch, useSelector } from 'react-redux';
-import { Outlet, useNavigate } from 'react-router-dom';
-import PageWrapper from '../templates/PageWrapper';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import PageWrapper from "../templates/PageWrapper";
 
 function AuthWrapper() {
-    return (
-			<PageWrapper>
-				<Outlet />
-			</PageWrapper>
-		
-	);
+  return (
+    <PageWrapper>
+      <Outlet />
+    </PageWrapper>
+  );
 }
 
-export default AuthWrapper
+export default AuthWrapper;
